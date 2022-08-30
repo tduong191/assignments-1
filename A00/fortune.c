d5d83b2554bf2d0a70e87ad683840c9107dbb932
Trang Duong <trangduong@wc137-dhcp-112.brynmawr.edu>
2022-08-30 15:16:34 -0400

    assignment 0 exercise

---
+++ A00/fortune.c
@@ -0,0 +1,13 @@
+/***************************************************
+ * hello.c
+ * Author: Trang Duong
+ * Date: 8/30/2022
+ * Print a fortune line
+ */
+
+#include <stdio.h>
+
+int main() {
+  printf("What a beautiful day!\n");
+  return 0;
+}
