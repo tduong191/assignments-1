eb66f8418d0effe5e09f4d217348a381503ad384
Trang Duong <tduong@goldengate.cs.brynmawr.edu>
2022-09-22 13:00:55 -0400

    assignment 2 complete

--- A02/2048.c
+++ A02/2048.c
@@ -1,5 +1,77 @@
 #include <stdio.h>
 
+int max(int num1, int num2) {
+    return (num1 > num2 ) ? num1 : num2;
+}
+
 int main() {
+  int board[4][4];
+  int max_val = 0;
+  // read text file into board
+  for (int i = 0; i < 4; i++) {
+    for (int j = 0; j < 4; j++) {
+      scanf("%d", &board[i][j]);
+      max_val = max(max_val, board[i][j]);
+    }
+  }
+
+  // loop horizontally
+  int row = 0;
+  int temp = 0;
+  int max_val_shift = 0;
+  while (row < 4) {
+    int col = 0;
+    while (col < 4) {
+      if (board[row][col] != 0) { 
+        temp = board[row][col]; // if temp is not 0 then find the check == temp
+        int check_col = col + 1;
+        while (check_col < 4) {
+          if (board[row][check_col] == temp) {
+            max_val_shift = max(max_val_shift, temp * 2);
+            check_col = 4;
+          } else if (board[row][check_col] != 0) {
+            check_col = 4;
+          } else {
+            check_col = check_col + 1;
+          }
+        }
+      }
+      col ++;
+    }
+    row ++;
+  }
+
+  // loop vertically
+  int col = 0;
+  row = 0;
+  while (col < 4) {
+    int row = 0;
+    while (row < 4) {
+      if (board[row][col] != 0) { 
+        temp = board[row][col]; // if temp is not 0 then find the check == temp
+        int check_row = row + 1;
+        while (check_row < 4) {
+          if (board[check_row][col] == temp) {
+            max_val_shift = max(max_val_shift, temp * 2);
+            check_row = 4;
+          } else if (board[check_row][col] != 0) {
+            check_row = 4;
+          } else {
+            check_row = check_row + 1;
+          }
+        }
+      }
+      row ++;
+    }
+    col ++;
+  }
+
+  // if there is no shift, return the max value in th board
+  if (max_val_shift != 0) {
+    printf("The largest value is %d.\n", max_val_shift);
+  } else {
+    printf("The largest value is %d.\n", max_val);
+  }
   return 0;
 }
+
--- A02/dynamic_restaurants.c
+++ A02/dynamic_restaurants.c
@@ -1,5 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h> 
+
+struct restaurant {
+    char name[32];
+    int open_time;
+    int close_time;
+    float rating;
+};
 
 int main() {
+  printf("How many restaurants do you want to enter? ");
+  int num_restaurants;
+  scanf("%d", &num_restaurants);
+  struct restaurant *my_restaurants = malloc(num_restaurants * sizeof(struct restaurant));
+  for (int i = 0; i < num_restaurants; i++) {
+    printf("Enter a name: ");
+    scanf(" %[^\n]%*c", my_restaurants[i].name);
+    printf("Open time: ");
+    scanf("%d", &my_restaurants[i].open_time);
+    printf("Close time: ");
+    scanf("%d", &my_restaurants[i].close_time);
+    printf("Stars: ");
+    scanf("%f", &my_restaurants[i].rating);
+  }
+  printf("Welcome to Dynamic Donna's Restaurant Directory.\n\n");
+
+  int i = 0;
+  while (i < num_restaurants) {
+    printf("%d)", i);
+    printf("   %3s  ", my_restaurants[i].name);
+    printf("open: %3d   ", my_restaurants[i].open_time);
+    printf("close: %3d   ", my_restaurants[i].close_time);
+    printf("stars: %3f   \n", my_restaurants[i].rating);
+    i++;
+  }
+  free(my_restaurants);
   return 0;
 }
+
